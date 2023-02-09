76bb436791afe635bbd21643c0a7cd002dc8ce9e
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-02-09 21:25:27 +0100

    commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,17 @@
-const Button = () => {
-  return <button className="btn">Add</button>;
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
 };
 
 export default Button;
