992809fd42c193944cda44303a291dba7caf5c27
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-02-15 11:54:36 +0100

    adds

--- src/App.js
+++ src/App.js
@@ -23,19 +23,43 @@ const App = () => {
     const res = await fetch("http://localhost:5000/tasks");
     const data = await res.json();
 
-    console.log(data);
+    return data;
+  };
+
+  // Fetch Task
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks`);
+    const data = await res.json();
+
     return data;
   };
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
   };
 
   //Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/task/${id}`, {
+      method: "DELETE",
+    });
+
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
