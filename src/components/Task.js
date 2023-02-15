2a7b5ffe4487a522303eeab33c0e3ac41cdb503a
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-02-15 09:34:31 +0100

    janet

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,13 +3,8 @@ import Todo from "./Todo";
 const Task = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
-      {tasks.map((task) => (
-        <Todo
-          key={task.id}
-          task={task}
-          onDelete={onDelete}
-          onToggle={onToggle}
-        />
+      {tasks.map((task, index) => (
+        <Todo key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
       ))}
     </>
   );
